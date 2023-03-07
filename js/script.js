cac453ac8c4d45f87c005914adc5f4b9a850db4a
bdancs <rm551645@fiap.com.br>
2023-03-07 11:51:47 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Este é um comentário de linha
+
+/**
+ * É um comentário
+ * de
+ * Bloco
+ */
+
+console.log("Olá mundo!")
+console.error("Bom dia")
+console.warn("Será?")
+console.info("Vai ser...")
